134deee085eb344f05011bf3ee69ed7f24322e40
Russell Martin <rmartin16@gmail.com>
2023-01-17 16:46:14 -0500

    Support building qBittorrent from an arbitrary repo (#20)
    - Add build args for specifying a source for qBittorrent
    - Use boost from alpine packages
    - Ensure CI builds use the base image built during CI

--- scripts/install_libtorrent.sh
+++ scripts/install_libtorrent.sh
@@ -4,14 +4,11 @@ set -e
 
 BASE_PATH="${1}"
 LIBTORRENT_VERSION="${2}"
-BOOST_DIR="${3}"
 
 cd "${BASE_PATH}"
-
 git clone --shallow-submodules --recurse-submodules https://github.com/arvidn/libtorrent.git "libtorrent"
 
 cd "${BASE_PATH}/libtorrent"
-
 if [ "${LIBTORRENT_VERSION}" = "v2-latest" ]; then
   git checkout "$(git tag -l --sort=-v:refname "v2*" | head -n 1)"
 elif [ "${LIBTORRENT_VERSION}" = "v1-latest" ]; then
@@ -23,7 +20,6 @@ fi
 cmake -Wno-dev -G Ninja -B build \
   -D CMAKE_BUILD_TYPE="Release" \
   -D CMAKE_CXX_STANDARD=17 \
-  -D BOOST_INCLUDEDIR="${BOOST_DIR}" \
   -D CMAKE_INSTALL_LIBDIR="lib" \
   -D CMAKE_INSTALL_PREFIX="/usr/local"
 cmake --build build --parallel "$(nproc)"
